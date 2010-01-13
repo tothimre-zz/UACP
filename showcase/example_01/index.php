ec9e7ecfca6461707f9be2c691f891fca7431866
Your Name <tothimre@gmail.com>
2010-01-13 19:14:29 +0100

    Added Apache 3 license to the project modifyed build at he showcase directory

--- showcase/example_01/index.php
+++ showcase/example_01/index.php
@@ -1,8 +1,23 @@
 <?php
-	/**
-	 * This is just a little example how to implement the framework the most easily and get
-	 * a full featured authentication module.
-	 */
+ /*Copyright 2010 Imre Toth <tothimre at gmail>
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+   */
+
+/**
+ * This is just a little example how to implement the framework the most easily and get
+ * a full featured authentication module.
+ */
 
 	session_start(); 
 	require 'classes_for_the_example.php';
